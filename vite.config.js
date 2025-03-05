dffc8e3b1a7c9d410157505f1865488a40c2d4a5
TarunThakur7974 <tarunsinghthakur6261@gmail.com>
2025-03-05 12:04:17 +0530

    vite config Threeee

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,36 @@
+// // import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
+// // export default defineConfig({
+//   // plugins: [
+//     // tailwindcss(),
+//   // ],
+// // })
+// // 
 // import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+// import path from 'path'
+
 // export default defineConfig({
-  // plugins: [
-    // tailwindcss(),
-  // ],
-// })
-// 
-import { defineConfig } from 'vite'
-import path from 'path'
-
-export default defineConfig({
-  plugins: [tailwindcss(),],
+//   plugins: [tailwindcss(),],
   
-  resolve: {
-    alias: {
-      // eslint-disable-next-line no-undef
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      external: [/^\/src\/assets\/.*/],
-      input: 'index.html',
-    },
-  },
-})
+//   resolve: {
+//     alias: {
+//       // eslint-disable-next-line no-undef
+//       '@': path.resolve(__dirname, './src'),
+//     },
+//   },
+//   build: {
+//     outDir: 'dist',
+//     rollupOptions: {
+//       external: [/^\/src\/assets\/.*/],
+//       input: 'index.html',
+//     },
+//   },
+// })
 
- 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
