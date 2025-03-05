41fbc1ceb27e0b2c99a09ab5312a246000af8b81
TarunThakur7974 <tarunsinghthakur6261@gmail.com>
2025-03-05 11:55:34 +0530

    vite config Two

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,30 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; // Ensure React plugin is included
-import path from 'path';
+// import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+// export default defineConfig({
+  // plugins: [
+    // tailwindcss(),
+  // ],
+// })
+// 
+import { defineConfig } from 'vite'
+import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],  // No need to add TailwindCSS here
-
+  plugins: [tailwindcss(),],
+  
   resolve: {
     alias: {
+      // eslint-disable-next-line no-undef
       '@': path.resolve(__dirname, './src'),
     },
   },
-
   build: {
-    outDir: 'dist', // Ensure build output goes to the correct folder
+    outDir: 'dist',
     rollupOptions: {
-      input: 'index.html', // Ensure the correct entry file
+      external: [/^\/src\/assets\/.*/],
+      input: 'index.html',
     },
   },
-});
+})
+
+ 
